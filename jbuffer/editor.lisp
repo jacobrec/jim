585f475392a4f5712fc012d5d71c6ddd41229658
Peter Elliott <pelliott@ualberta.ca>
2019-11-26 12:47:20 -0700

    integrated jbuffer into jim

---
+++ jbuffer/editor.lisp
@@ -0,0 +1,133 @@
+;;;; editor operations on immutable ropes
+;;;; author: Peter Elliott
+;;;; licence: LGPL-v3
+
+(defpackage :jbuffer-editor
+  (:nicknames jbedit)
+  (:use :cl)
+  (:export
+    #:open-buff
+    #:write-buff
+    #:insert-coord
+    #:insert
+    #:del-from-coord
+    #:del-from
+    #:del-coord-n
+    #:undo
+    #:redo
+    #:buffer))
+
+
+(in-package :jbuffer-editor)
+
+
+(defstruct buffer
+  stack  ; the stack of buffer snapshots (terminated when car is not list)
+  redo   ; the stack of undone snapshots
+  dirty  ; whether or not the buffer has been updated
+  fname) ; the file the buffer is associated with
+
+
+(defun buffer-head (buf)
+  (let ((stack (buffer-stack buf)))
+    (if (listp stack)
+      (car stack)
+      stack)))
+
+
+(defun fname-to-string (fname)
+  (with-open-file (strm fname)
+    (let ((contents (make-string (file-length strm))))
+      (read-sequence contents strm)
+      contents)))
+
+
+(defun open-buff (fname)
+  (make-buffer
+    :stack (jbrope:str-to-rope (fname-to-string fname))
+    :redo nil
+    :dirty nil
+    :fname fname))
+
+
+(defun write-buff (buff &optional fname)
+  (if (null fname) (setf fname (buffer-fname buff)))
+
+  (with-open-file (f fname
+                     :direction :output
+                     :if-exists :supersede
+                     :if-does-not-exist :create)
+    (mapcar
+      (lambda (chunk)
+        (princ chunk f))
+      (jbrope:chunks (buffer-head buff))))
+
+  (make-buffer
+    :stack (buffer-stack buff)
+    :redo  (buffer-redo buff)
+    :dirty nil
+    :fname (buffer-fname buff)))
+
+
+(defun insert-coord (buff str lines &optional (cols 0))
+  (insert buff str (jbrope:coord-to-idx lines cols)))
+
+
+(defun insert (buff str i)
+  (make-buffer
+    :stack (cons
+             (jbrope:insert
+               (buffer-head buff)
+               (jbrope:str-to-rope str)
+               i)
+             (buffer-stack buff))
+    :redo nil
+    :dirty t
+    :fname (buffer-fname buff)))
+
+
+(defun del-from-coord (buff s-line e-line &optional (s-cols 0) (e-cols 0))
+  (del-from buff
+            (jbrope:coord-to-idx s-line s-cols)
+            (jbrope:coord-to-idx e-line e-cols)))
+
+
+(defun del-from (buff start end)
+  (make-buffer
+    :stack (cons
+             (jbrope:del-from (buffer-head buff) start end)
+             (buffer-stack buff))
+    :redo nil
+    :dirty t
+    :fname (buffer-fname buff)))
+
+
+(defun del-coord-n (buff sline scol n)
+  (let ((i (jbrope:coord-to-idx sline scol)))
+    (if (> n 0)
+      (del-from buff i (+ i n))
+      (del-from buff (+ i n) i))))
+
+
+(defun undo (buff)
+  (if (listp (buffer-stack buff))
+    (make-buffer
+      :stack (cdr (buffer-stack buff))
+      :redo  (cons
+               (car (buffer-stack buff))
+               (buffer-redo buff))
+      :dirty t  ;TODO: attach dirtyness to snapshot
+      :fname (buffer-fname buff))
+    buff))
+
+
+(defun redo (buff)
+  (if (buffer-redo buff)
+    (make-buffer
+      :stack (cons
+               (car (buffer-redo buff))
+               (buffer-stack buff))
+      :redo  (cdr (buffer-redo buff))
+      :dirty t ;TODO: attatch dirtyness to snapshot
+      :fname (buffer-fname buff))
+    buff))
---
+++ jbuffer/istring.lisp
@@ -0,0 +1,58 @@
+;;;; immutable strings with O(1) substrings
+;;;; author: Peter Elliott
+;;;; licence: LGPL-v3
+
+(defpackage :jbuffer-istring
+  (:nicknames jbstring)
+  (:use :cl)
+  (:export
+    #:make-istring))
+
+(in-package :jbuffer-istring)
+
+
+(defclass istring (sequence standard-object)
+  ((base  :accessor istring-base
+          :initarg  :base)
+   (start :accessor istring-start
+          :initarg  :start)
+   (end   :accessor istring-end
+          :initarg  :end)))
+
+
+(defun make-istring (str)
+  (make-instance 'istring
+                 :base  str
+                 :start 0
+                 :end   (length str)))
+
+
+(defmethod sb-sequence:elt ((seq istring) index)
+  (elt (istring-base seq) (+ (istring-start seq) index)))
+
+
+(defmethod sb-sequence:length ((seq istring))
+  (- (istring-end seq) (istring-start seq)))
+
+
+(defmethod sb-sequence:subseq ((seq istring) start &optional end)
+  (make-instance 'istring
+                 :base  (istring-base seq)
+                 :start (+ start (istring-start seq))
+                 :end   (if end
+                          (+ end (istring-start seq))
+                          (istring-end seq))))
+
+
+(defmethod print-object ((obj istring) stream)
+  (cond
+    (*print-escape* (progn
+                      (princ #\" stream)
+                      (istring-princ obj stream)
+                      (princ #\" stream)))
+    (t (istring-princ obj stream))))
+
+
+(defun istring-princ (istr stream)
+  (map nil (lambda (ch) (princ ch stream)) istr))
+
