ba0920d0750a679832df32162637df6ccfa54f88
Jacob Reckhard <jacobreckhard@gmail.com>
2019-11-27 11:18:33 -0700

    made bash script better

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ if [ "$1" = "test" ]; then
     echo "" | sbcl --load test_runner.lisp
 else
     sbcl --script appLaunch.lisp 2> crash;
-    reset
-    tput rmcup
-    echo -en "\e[?25h"
-    cat crash
+    if [ $? -ne 0 ]; then
+        reset
+        tput rmcup
+        echo -en "\e[?25h"
+        cat crash
+        echo "Terminal fixed after crash"
+    fi
 fi
