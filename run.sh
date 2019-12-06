9fa88a2d92947716725c1af2674f7841d18bb5eb
Jacob Reckhard <jacobreckhard@gmail.com>
2019-12-06 13:47:41 -0700

    added remove cache script

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 if [ "$1" = "test" ]; then
     echo "" | sbcl --load test_runner.lisp
+elif [ "$1" = "cache" ]; then
+    rm -rf ~/.cache/common-lisp/
+    ./run.sh "${@:2}"
 else
     sbcl --script appLaunch.lisp "$@" 2> crash;
     if [ $? -ne 0 ]; then
